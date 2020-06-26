6cd8dd2cc6064bc41ea2e4900e436d965d0b6dce
Đức Anh Tài <39086667+datai999@users.noreply.github.com>
2020-06-27 00:34:33 +0700

    first commit

---
+++ src/main/java/com/hcmut/scrum/WebRentRoomApplication.java
@@ -0,0 +1,13 @@
+package com.hcmut.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRentRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRentRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmut/scrum/WebRentRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmut.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRentRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
